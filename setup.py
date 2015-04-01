24f73777661f7f9338b4f48130b823231fae29a5
Piotr Jarolewski <jarolewski.piotr@gmail.com>
2015-04-01 21:20:21 +0200

    Configuration project

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='osfakt',
+    version='0.1',
+    packages=['src.osfakt'],
+    include_package_data=True,
+    license='BSD License',
+    description='Django apps to invoice',
+    long_description=README,
+    url='http://www.webcoding.pl',
+    author='Piotr Jarolewski',
+    author_email='jarolewski.piotr@gmail.com',
+    classifiers=[
+        'Framework :: Django',
+        ],
+    install_requires=[
+        'django>=1.7',
+    ]
+)
---
+++ src/osfakt/main/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'osfakt.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
